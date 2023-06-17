1e5bd062b72a167acee60cf03dfa8b990721b47d
Tomasz Pinter <tomaszpinter@gmail.com>
2023-06-17 11:51:27 +0200

    serviceworker

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,3 +85,11 @@ let searchPosition = (position) => {
    weatherForm.requestSubmit()
 
 }
+if ("serviceWorker" in navigator) {
+	window.addEventListener("load", function () {
+    	navigator.serviceWorker
+        	.register("serviceWorker.js")
+        	.then(res => console.log("service worker registered"))
+        	.catch(err => console.log("service worker not registered", err))
+	})
+}
